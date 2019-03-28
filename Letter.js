14b1f691cd03dfe3b96e2ab65bde4c0aa3877e12
nellie-geek <janellegieck@msn.com>
2019-03-28 17:10:18 -0600

    Inquirer install. Updated js with functionality.

---
+++ Letter.js
@@ -0,0 +1,22 @@
+// Letter constructor - display word as dashes for the correct number of characters in the word//
+function Letters(letter) {
+    this.letter = letter,
+    this.guessed = false,
+    this.displayLetter = function() {
+        if (this.letter === " ") {
+            return " ";
+        }else if(!this.guessed) {
+            return "_";
+        } else {
+            return this.letter;
+        }
+    }
+//function updates boolean so the letter guessed is displayed//
+    this.check = function(userGuess) {
+        if(userGuess === this.letter) {
+            this.guessed = true;
+        }
+    }
+}
+
+module.exports = Letters;
---
+++ Word.js
@@ -0,0 +1,28 @@
+//dependent on letter.js 
+var Letters = require("./Letter");
+
+//Word constructor
+function Word(wordArr) {
+    this.wordArr = wordArr;
+    this.guessWord = [];
+    this.makeWord = function() {
+        for (var i = 0; i < wordArr.length; i++) {
+            var letter = new Letters(wordArr[i]);
+            this.guessWord.push(letter);
+        }
+    }
+    this.showWord = function() {
+        var wordDis = [];
+        for(var i = 0; i < this.guessWord.length; i++) {
+            wordDis.push(this.guessWord[i].displayLetter());
+        }
+        return wordDis.join(" ");
+    }
+    this.checkGuess = function(userGuess) {
+        for (var i = 0; i < this.guessWord.length; i++) {
+            this.guessWord[i].check(userGuess);
+        }
+    }
+}
+
+module.exports = Word; 
